caa95d74473e6146fa7934d2550b09a21d682217
koladeore <koladeore@gmail.com>
2022-09-21 13:08:41 +0100

    ft-public-private-readonly

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
+// classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor(c: string, d: string, a: number){
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string, 
+    private details: string, 
+    public amount: number,
+  ){}
 
   format() {
     return `${this.client} owes £${this.amount} for ${this.details}`;
@@ -20,10 +21,10 @@ const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices: Invoice[] = [];
 invoices.push(invOne)
 invoices.push(invTwo);
-// invoices.push({ name: 'shaun' });
 
-console.log(invoices);
-
+invoices.forEach(inv => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+})
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
